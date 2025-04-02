efb9814e844e872a18a80b3887ecb25e35f11fa6
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2025-04-02 09:58:50 -0400

    removendo comentarios

--- src/app/components/forms/ProgressBar.tsx
+++ src/app/components/forms/ProgressBar.tsx
@@ -1,40 +1,49 @@
-import { Box, HStack, Text } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { Box, HStack, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const MotionBox = motion(Box);
 
 type ProgressBarProps = {
   currentStep: number;
   totalSteps: number;
-  indicatorPosition: 'top-left' | 'top' | 'top-right' | 'bottom-left' | 'bottom' | 'bottom-right';
+  indicatorPosition:
+    | "top-left"
+    | "top"
+    | "top-right"
+    | "bottom-left"
+    | "bottom"
+    | "bottom-right";
   width?:
     | string
     | number
-    | { base: string | number; sm?: string | number; md?: string | number; lg?: string | number; xl?: string | number }; // Define a largura da barra, pode ser string, número ou objeto de breakpoints
+    | {
+        base: string | number;
+        sm?: string | number;
+        md?: string | number;
+        lg?: string | number;
+        xl?: string | number;
+      };
 };
 
 export default function ProgressBar({
   currentStep,
   totalSteps,
   indicatorPosition,
-  width = { base: '200px' }, // Valor padrão para a largura como breakpoint
+  width = { base: "200px" },
 }: ProgressBarProps) {
-  // Move o cálculo do progressPercentage para o componente ProgressBar
   const progressPercentage = (currentStep / totalSteps) * 100;
 
-  const bgColor = progressPercentage === 100 ? 'green.400' : 'blue.500'; // Verde se 100%, azul caso contrário
+  const bgColor = progressPercentage === 100 ? "green.400" : "blue.500";
 
-  // Define o posicionamento do indicador
   const positionStyles = {
-    'top-left': { top: '-24px', left: '0' },
-    top: { top: '-24px', left: '50%', transform: 'translateX(-50%)' },
-    'top-right': { top: '-24px', right: '0' },
-    'bottom-left': { bottom: '-24px', left: '0' },
-    bottom: { bottom: '-24px', left: '50%', transform: 'translateX(-50%)' },
-    'bottom-right': { bottom: '-24px', right: '0' },
+    "top-left": { top: "-24px", left: "0" },
+    top: { top: "-24px", left: "50%", transform: "translateX(-50%)" },
+    "top-right": { top: "-24px", right: "0" },
+    "bottom-left": { bottom: "-24px", left: "0" },
+    bottom: { bottom: "-24px", left: "50%", transform: "translateX(-50%)" },
+    "bottom-right": { bottom: "-24px", right: "0" },
   };
 
-  // Exibe o componente apenas a partir da terceira pergunta
   if (currentStep < 3) {
     return <div />;
   }
@@ -46,14 +55,14 @@ export default function ProgressBar({
       bg="gray.700"
       borderRadius="md"
       position="relative"
-      initial={{ opacity: 0 }} // Estado inicial da animação
-      animate={{ opacity: 1 }} // Estado final da animação
-      transition={{ duration: 0.5 }} // Duração da animação
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
     >
       <Box
         height="100%"
         width={`${progressPercentage}%`}
-        bg={bgColor} // Cor dinâmica
+        bg={bgColor}
         borderRadius="md"
         transition="width 0.3s ease"
       />
@@ -64,7 +73,7 @@ export default function ProgressBar({
         justifyContent="space-between"
         position="absolute"
         fontSize={{ base: 14, sm: 16 }}
-        {...positionStyles[indicatorPosition]} // Aplica o estilo de posicionamento
+        {...positionStyles[indicatorPosition]}
       >
         <Text as="small">{currentStep}</Text>
         <Text as="small">de</Text>
